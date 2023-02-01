b6a068021f5814d424ebfd4e535e6d7e0c6b95a6
what578 <ryan_merge@hotmail.com>
2023-02-01 03:02:05 -0600

    box now properly renders

--- display.py
+++ display.py
@@ -1,6 +1,8 @@
 import vector
 import colorama
 import time
+import msvcrt
+#import curses
 
 
 
@@ -29,31 +31,40 @@ FPS          = 10
 #vector2= (4,4)
 #topLeft= ()
 #bottomR= (4,)
+#def box(center,size):
+#    vector2 = center.sub(vector.Vector(size,size))
+#    vector1 = center.sum(vector.Vector(size,size))
+#    #print(f"center is {center.x} {center.y}")
+#    #print(f"x for 1 and 2 ={vector1.x} {vector2.x} ")
+#    #print(f"y for 1 and 2 ={vector1.y} {vector2.y}")
+#    for x in range(vector1.x - vector2.x):
+#        for y in range(vector1.y -vector2.y):
+#            value = (x+vector2.x) + ((y+vector2.y)*WIDTH)
+#            value1= x+vector2.x
+#            value2= (y+vector2.y)*WIDTH
+#            if 0 <= value and value <= HEIGHT*WIDTH:
+#                display[value] = "Q"
+#
 def box(center,size):
-    vector1 = center.sum(vector.Vector(size,size))
     vector2 = center.sub(vector.Vector(size,size))
-    #print(f"center is {center.x} {center.y}")
-    #print(f"x for 1 and 2 ={vector1.x} {vector2.x} ")
-    #print(f"y for 1 and 2 ={vector1.y} {vector2.y}")
-    for x in range(vector1.x - vector2.x +1):
-        for y in range(vector1.y -vector2.y +1):
-            value = (x+vector2.x) + ((y+vector2.y)*WIDTH)
-            value1= x+vector2.x
-            value2= (y+vector2.y)*WIDTH
-            #if value1 >31 or value1 < 0:
-            #
-            #if value2 >31 or value2 < 0:
-            #    continue
-            if 0 <= value and value <= HEIGHT*WIDTH:
-                display[value] = "Q"
+    vector1 = center.sum(vector.Vector(size,size))
+    x = vector2.x
+    y = vector2.y
+    while y <= vector1.y:
+        while x <= vector1.x:
+            if x >= 0 and x < WIDTH and y >= 0 and y < HEIGHT:
+                display[(y*WIDTH) + x] = "Q"
+            x += 1
+        y += 1
+        x = vector2.x
 
 
 
 
 
-def fill():
+def fill(filler):
     for x in range(WIDTH*HEIGHT):
-        display[x] = " "
+        display[x] =  filler
 
 def dot():
     x = 31
@@ -69,31 +80,25 @@ def show():
         print("".join(display[startRow:endRow]))
         startRow += WIDTH
         endRow   += WIDTH
-    move(0,0)
 
 if __name__ == "__main__":
     colorama.init()
-    boxC = vector.Vector(5,2)
+    filler="*"
+    boxC = vector.Vector(5,0)
     vel  = vector.Vector(0,1)
     size = 2
     quit = False
-    while not quit:
-        fill()
-        if boxC.y >= HEIGHT -size*2:
-            vel.setY(vel.y*-.9)
-            boxC = boxC.sum(vel)
-        #elif boxC.y <= HEIGHT - size:
-        #    vel.setY(vel.y*-1)
-        #    boxC = boxC.sum(vel)
-        elif boxC.y <= 0 :
-            vel.setY(vel.y*-1)
-            boxC = boxC.sum(vel)
+    char = None
 
-
-        else:
-            boxC = boxC.sum(vel)
-
-
-        box(boxC,size)
-        show()
-        time.sleep(1/FPS)
+    fill(filler)
+    box(boxC,size)
+    show()
+    #while not quit:
+    #    char = msvcrt.getch()
+    #    if char == "q":
+    #        quit = True
+    #    print(char)
+        #C = boxC.sum(vel)
+        #box(boxC,size)
+        #show()
+        #time.sleep(1/FPS)
