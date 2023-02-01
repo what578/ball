d01ce8dc6808516017956807cd60e49c82e9d455
what578 <ryan_merge@hotmail.com>
2023-02-01 05:42:09 -0600

    added mult,floor,ceil methods to Vector

--- vector.py
+++ vector.py
@@ -1,13 +1,17 @@
-
+import math
 
 class Vector:
 
     x       = 0.0
     y       = 0.0
 
-    def __init__(self,x,y):
-        self.x      = x
-        self.y      = y
+    def __init__(self,x,y =None):
+        if y == None:
+            self.x      = x
+            self.y      = x
+        else:
+            self.x      = x
+            self.y      = y
 
     def setX(self,num):
         self.x = num
@@ -17,3 +21,11 @@ class Vector:
         return Vector(self.x + vector.x, self.y + vector.y)
     def sub(self,vector):
         return Vector(self.x - vector.x, self.y - vector.y)
+    def mult(self,vector):
+        return Vector(self.x * vector.x,self.y * vector.y)
+    def floor(self):
+        self.x = math.floor(self.x)
+        self.y = math.floor(self.y)
+    def ceil(self):
+        self.x = math.ceil(self.x)
+        self.y = math.ceil(self.y)
