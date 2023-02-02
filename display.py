319ec29334dbd9e1726cc3145c7f300ace08f05e
what578 <ryan_merge@hotmail.com>
2023-02-01 21:40:24 -0600

    added class Display and basic display setup

--- display.py
+++ display.py
@@ -15,6 +15,58 @@ GRAVITY      = 5
 WALLDAMPER   = -1.0
 
 
+class Display:
+    fps         = None
+    #store as vector
+    dt          = None
+    height      = None
+    width       = None
+    #
+    display     = None
+    #dictionary of environment variable
+    environmentVariable = None
+    geometry    = []
+
+    def __init__(self,fps,height,width,dt,environmentVariable = {},geometry = [] ):
+        colorama.init()
+        self.fps        = fps
+        self.height     = height
+        self.width      = width
+        self.display    = [None] *(height*width)
+        self.dt         = dt
+        self.environmentVariable = environmentVariable
+        self.geometry   = geometry
+    def fill(self):
+        filler = " "
+        try:
+            filler = self.environmentVariable["filler"]
+        except:
+            print("")
+
+        for row in range(self.height):
+            for column in range(self.width):
+                self.display[row*self.width + column] = filler
+    def moveCursor(self):
+        print("\033[H")
+        #print("\033[%dA;%dD" % (0,0))
+        #print("\033[%d;%dH" % (self.height, self.width))
+
+
+
+    def show(self):
+        startRow,endRow = 0,self.width
+        for row in range(self.height):
+            print("".join(self.display[startRow:endRow]))
+            startRow += self.width
+            endRow   += self.width
+    def run(self):
+        quit = False
+        while not quit:
+            self.fill()
+            self.moveCursor()
+            self.show()
+            time.sleep(1/self.fps)
+
 
 #------------
 #---HHH------
@@ -89,42 +141,47 @@ def show():
         endRow   += WIDTH
 
 if __name__ == "__main__":
-    colorama.init()
-    filler="*"
-    pos = vector.Vector(WIDTH/2,3)
-    size = 2
+    #def __init__(self,fps,height,width,dt,environmentVariable = None):
+    environmentVariables = {"filler":"*"}
+    dis         =Display(10,32,32,vector.Vector(1/10,1/10),environmentVariables)
+    dis.run()
+    #colorama.init()
+    #filler="*"
+    #pos = vector.Vector(WIDTH/2,3)
+    #size = 2
 
-    vel  = vector.Vector(5,5)
-    gravity = vector.Vector(0,GRAVITY)
-    dtV = vector.Vector(DT)
-    quit = False
-    while not quit:
-        vel = vel.sum(gravity.mult(dtV))
-        pos = pos.sum(vel.mult(dtV))
+    #vel  = vector.Vector(5,5)
+    #gravity = vector.Vector(0,GRAVITY)
+    #dtV = vector.Vector(DT)
+    #quit = False
+    #while not quit:
+    #    vel = vel.sum(gravity.mult(dtV))
+    #    pos = pos.sum(vel.mult(dtV))
 
-        if pos.y > HEIGHT - (size+1):
-            pos.y = HEIGHT - (size+1)
-            vel.y *= WALLDAMPER
-        elif pos.y < 0 + size +1:
-            pos.y = 0 + (size+1)
-            vel.y *= WALLDAMPER
-        if pos.x > WIDTH - (size+1):
-            pos.x = WIDTH - (size+1)
-            vel.x *= WALLDAMPER
-        elif pos.x < 0 + size + 1:
-            pos.x = 0 + (size +1)
-            vel.x *= WALLDAMPER
-
-
+    #    if pos.y > HEIGHT - (size+1):
+    #        pos.y = HEIGHT - (size+1)
+    #        vel.y *= WALLDAMPER
+    #    elif pos.y < 0 + size +1:
+    #        pos.y = 0 + (size+1)
+    #        vel.y *= WALLDAMPER
+    #    if pos.x > WIDTH - (size+1):
+    #        pos.x = WIDTH - (size+1)
+    #        vel.x *= WALLDAMPER
+    #    elif pos.x < 0 + size + 1:
+    #        pos.x = 0 + (size +1)
+    #        vel.x *= WALLDAMPER
 
 
 
-        fill(filler)
-        moveCursor()
-        box(pos,size)
-        show()
-        time.sleep(1/FPS)
+
+
+    #    fill(filler)
+    #    moveCursor()
+    #    box(pos,size)
+    #    show()
+    #    time.sleep(1/FPS)
         #move(1,1)
+
     #while not quit:
     #    char = msvcrt.getch()
     #    if char == "q":
